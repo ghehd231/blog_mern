37a501e0ce1cc05230b9d2bdc065b7726fae0fba
ghehd231 <ghehd231@naver.com>
2019-04-20 15:29:47 +0900

    user register

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -1,5 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const userModel = require('../../models/userModel');
+
+/**
+ * @route   POST api/user/register
+ * @desc    Register user
+ * @access  Public
+ */
+router.post('/register', (req, res) => {
+    userModel
+        .findOne({email: req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(400).json({
+                    msg: 'Email already exists'
+                });
+            }else{
+                // Create avatar
+                // http://www.gravatar.com/avatar/56a0311c22d8e7b6d152ca6b90aa2e46?s=200&r=pg&d=mm 대충 이런식으로 주소가 만들어짐.
+                // 위의 주소가 DB에 들어감
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200',   // size
+                    r: 'pg',    // Rating
+                    d: 'mm'     // Default
+                });
+
+                // Create User
+                const newUser = new userModel({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+
+                // 암호화
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err; // 암호화 실패하면 에러던짐
+                        newUser.password = hash; // 성공하면 비밀번호에 해쉬값 넣음
+                        newUser.save() // 저장
+                            .then(user => res.json(user))
+                            .catch(err => res.json(err));
+                    });
+                });
+            }
+        })
+        .catch(err => res.json(err))
+});
 
 /**
  * @route   GET api/user/test
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')// request.body 쓸수 있음
+
 
 /** routing 경로 */
 const userRouter = require('./routes/api/userRouter');
@@ -7,6 +9,15 @@ const profileRouter = require('./routes/api/profileRouter');
 const postRouter = require('./routes/api/postRouter');
 
 const app = express();
+
+
+/**
+ * @desc    body-parser setting
+ * @access  Public
+ */
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 /** use route */
 app.use('/api/user', userRouter);
@@ -21,11 +32,11 @@ app.use('/api/post', postRouter);
  */
 const db = require('./config/keys').mongoURI;
 mongoose.Promise = global.Promise;
-mongoose.connect(db, { userNewUrlParser: true, userCreateIndex: true})
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true})
     .then( () => console.log('MongoDb Connected..'))
     .catch(err => console.log('err'));
 
-    
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
