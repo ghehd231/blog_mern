ba4f429e940cad836f42b306a558adf7851ce723
ghehd231 <ghehd231@naver.com>
2019-06-15 14:09:25 +0900

    post create routes

--- routes/api/postRouter.js
+++ routes/api/postRouter.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const postModle = require('../../models/postModel');
+
+//validation
+const validatePostInput = require('../../validation/post');
+
+//auth
+const authCheck = passport.authenticate('jwt', {session: false});
+
 
 /**
  * @route   GET api/post/test
@@ -8,4 +19,30 @@ const router = express.Router();
  */
 router.get('/test', (req, res) => res.json({msg: 'PostRouter Works'}));
 
+
+/**
+ * @route   Post api/post
+ * @desc    Create route
+ * @access  Private
+ */
+router.post("/",authCheck, (req, res) => {
+    const {errors, isValid} = validatePostInput(req.body);
+
+    //check validation
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
+    const newPost = new postModle({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    });
+
+    newPost.save()
+        .then(post => res.json(post))
+        .catch(err => res.status(400).json(err));
+        
+})
 module.exports = router;
